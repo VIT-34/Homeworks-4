328d843bf4fbec3174cac50529a2bcb689f446ca
Vitaliy <viurum986@mail.ru>
2022-03-22 21:20:36 +0300

    Task 0104

---
+++ Task 0104/Program.cs
@@ -0,0 +1,18 @@
+﻿
+int a, b;
+int pow = 1;
+Console.Write("Введите число: ");
+a = Int32.Parse(Console.ReadLine());
+Console.Write("введите стпень числа: ");
+b = Int32.Parse(Console.ReadLine());
+
+
+for (int i = 0; i < b; i++)
+{
+    pow *= a;
+}
+Console.WriteLine($"Степень числа {pow}");
+return 0;
+
+
+
